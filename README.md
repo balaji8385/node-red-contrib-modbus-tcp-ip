96f27a8c3988ae38ecbbfb9efa7894e45100fa62
Balaji L Narayanan <lbalaji@live.com>
2020-03-05 10:58:46 +0530

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Node-RED Modbus TCP Flexi Reader
+
+This package enables the user to read data from the modbus enabled devices through TCP/IP.
+
+## Getting Started
+
+npm install this module and you can find the block. Example node-red code can be found this repo.
+
+## Authors
+
+* **Balaji L Narayanan** - *Initial work* - [balaji8385](https://github.com/balaji8385)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ modbus-tcp-ip.js
@@ -0,0 +1,152 @@
+const modbus = require("modbus-stream");
+
+let connection;
+let getModbusConnection = (ip, port) => {
+    return new Promise((resolve, reject) => {
+        modbus.tcp.connect(port, ip, {
+            debug: null,
+            connectTimeout: 3000
+        }, (err, connection) => {
+            if (!err) resolve(connection);
+            else reject(err)
+        })
+    })
+}
+
+
+let startTime, endTime;
+
+function start() {
+    startTime = new Date();
+};
+
+function end() {
+    endTime = new Date();
+    let timeDiff = endTime - startTime;
+    timeDiff /= 1;
+    let seconds = Math.round(timeDiff);
+    return seconds;
+}
+
+module.exports = function (RED) {
+    function ModbusTcpIpNode(config) {
+        RED.nodes.createNode(this, config);
+        this.ip = config.ip;
+        this.port = config.port;
+        var node = this;
+        node.on('input', async function (msg, send, done) {
+            msg = msg;
+            msg.payload = msg.payload;
+            msg.ip = node.ip;
+            msg.port = parseInt(node.port);
+
+            if (!msg.payload.address && msg.payload.address !== 0) {
+                done("Invaid Modbus Address");
+            } else if (!msg.payload.quantity) {
+                done("Invaid Modbus Quantity");
+            } else if (!msg.payload.unitid) {
+                done("Invaid Modbus Unit ID");
+            } else {
+                // if (!connection) {
+                try {
+                    node.status({
+                        fill: "yellow",
+                        shape: "dot",
+                        text: "Connecting to MODBUS TCP/IP"
+                    });
+                    connection = await getModbusConnection(node.ip, node.port);
+                    // node.log("Connection Established");
+                    connection.on('error', (err) => {
+                        node.log(`Re-Connecting to ${node.ip}:${node.port}`);
+                    });
+                    node.status({
+                        fill: "green",
+                        shape: "dot",
+                        text: "Connection Established"
+                    });
+                } catch (err) {
+                    node.error(err, err.message);
+                    node.status({
+                        fill: "red",
+                        shape: "dot",
+                        text: "Connection Error"
+                    })
+                    done();
+                }
+                // }
+
+                if (connection) {
+                    start();
+                    node.status({
+                        fill: "yellow",
+                        shape: "dot",
+                        text: "Sending Request"
+                    })
+
+                    if (msg.payload.functioncode == 1) {
+                        connection.readCoils({
+                            address: msg.payload.address,
+                            quantity: msg.payload.quantity,
+                            extra: {
+                                unitId: msg.payload.unitid
+                            }
+                        }, responseCallBack(err, res))
+                    } else if (msg.payload.functioncode == 2) {
+                        connection.readDiscreteInputs({
+                            address: msg.payload.address,
+                            quantity: msg.payload.quantity,
+                            extra: {
+                                unitId: msg.payload.unitid
+                            }
+                        }, responseCallBack(err, res))
+                    } else if (msg.payload.functioncode == 3) {
+                        connection.readHoldingRegisters({
+                            address: msg.payload.address,
+                            quantity: msg.payload.quantity,
+                            extra: {
+                                unitId: msg.payload.unitid
+                            }
+                        }, responseCallBack(err, res))
+                    } else if (msg.payload.functioncode == 4) {
+                        connection.readInputRegisters({
+                            address: msg.payload.address,
+                            quantity: msg.payload.quantity,
+                            extra: {
+                                unitId: msg.payload.unitid
+                            }
+                        }, responseCallBack(err, res))
+                    }
+
+                    let responseCallBack = (err, res) => {
+                        if (!err) {
+                            node.status({
+                                fill: "green",
+                                shape: "dot",
+                                text: "Response Received " + end() + " ms"
+                            })
+                            msg.responseBuffer = {};
+                            msg.responseBuffer.buffer = Buffer.concat(res.response.data);
+                            connection.close(() => {
+                                node.log("Connection closed");
+                            });
+                            send(msg);
+                            done();
+                        } else {
+                            node.status({
+                                fill: "red",
+                                shape: "dot",
+                                text: "Error Getting Response"
+                            })
+                            node.error(err, err.message)
+                            connection.close(() => {
+                                node.log("Connection closed");
+                            });
+                            done();
+                        }
+                    }
+                }
+            }
+        });
+    }
+    RED.nodes.registerType("modbus-tcp-ip", ModbusTcpIpNode);
+}
